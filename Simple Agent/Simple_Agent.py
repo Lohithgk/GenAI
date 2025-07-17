c955237eb3340b524236d6e245b79281db34fe12
Lohith G K <86795049+Lohithgk@users.noreply.github.com>
2025-07-17 22:40:39 +0530

    Simple_Agent.py

--- Simple Agent/Simple_Agent.py
+++ Simple Agent/Simple_Agent.py
@@ -6,9 +6,9 @@ from agno.models.ollama import Ollama
 
 # Create the agent
 agent = Agent(
-    model=Ollama(id = "llama3.2", 
-                 provider = "Ollama", 
-                 system_prompt = "You are a helpful assistant, based on the input, answer in markdown format."),
+    model=Ollama(id="llama3.2", 
+                 provider="Ollama", 
+                 system_prompt="You are a helpful assistant, based on the input, answer in markdown format."),
     markdown=True)
 
 # Print the response in the terminal
